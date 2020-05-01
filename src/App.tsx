cd5112bf131a4f46a91dd0776f0a276320d0119c
Leonardo Kameya <leonardo.kameya@gmail.com>
2020-05-01 16:24:19 -0400

    Add listener to switch between dark or light mode depending on the SO configuration

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,19 @@ import { ThemeProvider, DefaultTheme } from 'styled-components';
 import usePeristedState from './utils/usePersistedState'
 
 import light from './styles/themes/light';
-import dark from './styles/themes/dark';   
+import dark from './styles/themes/dark';
 
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 
 const App = () => {
   const [theme, setTheme] = usePeristedState<DefaultTheme>('theme', light);
+
+  const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+  darkModeMediaQuery.addListener((e) => {
+    const darkModeOn = e.matches;
+    setTheme(darkModeOn ? dark : light);
+  });
 
   const toggleTheme = () => {
     setTheme(theme.title === 'light' ? dark : light);
